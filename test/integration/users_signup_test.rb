df0c1e26278f789e00c26aaaffa84a1777fdd6ec
Timo Lohmann <lohmanntimo@gmail.com>
2019-09-13 14:00:07 +0200

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference "User.count" do
+      post users_path, params: { user: {name: "",email: "user@invalid", password: "foo", password_confirmation: "bar"}}
+    end
+    assert_template "users/new"
+    assert_select '#error_explanation'
+    assert_select '.alert'
+    assert_select 'form[action="/signup"]'
+
+  end
+
+  test 'valid user data' do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: {name: "Test Tester",email: "test@test.de", password: "foobar", password_confirmation: "foobar"}}
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert_not flash.empty?
+    assert_select ".alert-success"
+
+  end
+
+
+
+
+end
