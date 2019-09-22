ac1e7771ac940bbb2d1e6cbaaffbcc4e851dcae0
Timo Lohmann <lohmanntimo@gmail.com>
2019-09-22 10:31:26 +0200

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = 'Welcome to the sample App!'
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = 'Bitte überprüfen Sie ihr Postfach um den Account !'
+      redirect_to root_url
     else
       render 'new'
     end
